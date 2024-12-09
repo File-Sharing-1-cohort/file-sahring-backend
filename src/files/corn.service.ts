ccedb8088ee032abdadb459845056b332cb776ba
Oleksii Hnatiienko <ohnatiienko@auvik.com>
2024-12-09 13:02:10 +0200

    logic for deleting lines older that 24h

--- src/files/corn.service.ts
+++ src/files/corn.service.ts
@@ -1,12 +1,27 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, LessThan } from 'typeorm';
+import { TransferredFile } from './entities/file.entity.js';
 
 @Injectable()
 export class CronService {
   private readonly logger = new Logger(CronService.name);
 
+  constructor(
+    @InjectRepository(TransferredFile)
+    private readonly fileRepository: Repository<TransferredFile>,
+  ) {}
+
   @Cron('* * * * *')
-  handleCron() {
+  async handleCron() {
     console.log(new Date());
+
+    const oneDayAgo = new Date();
+    oneDayAgo.setHours(oneDayAgo.getHours() - 24);
+
+    const result = await this.fileRepository.delete({
+      loadedAt: LessThan(oneDayAgo),
+    });
   }
 }
